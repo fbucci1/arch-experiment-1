4b3880b1d40c69a2b5b073abba808437e1762780
fbucci1 <fernandobucci@gmail.com>
2022-08-20 01:52:57 +0200

    Installation npm and nodejs

--- support/localEnvironment/kubernetesVM/resources/npm/install.sh
+++ support/localEnvironment/kubernetesVM/resources/npm/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt install -y nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+#Installs npm 14.15.0. This is the latest version compatible with angular-cli as of Aug 2022.
+nvm install v14.15.0
+
 echo "npm $(npm -v)"
 echo "node $(node -v)"
+
+npm cache clean --force
