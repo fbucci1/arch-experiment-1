6bd09ec5e463510a2e707a47d21733140c8a14b5
fbucci1 <fernandobucci@gmail.com>
2022-08-21 23:22:43 +0200

    sticks and stones

--- support/localEnvironment/kubernetesVM/code/backend/apis/src/main/java/com/example/apis/controllers/TestController.java
+++ support/localEnvironment/kubernetesVM/code/backend/apis/src/main/java/com/example/apis/controllers/TestController.java
@@ -8,8 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+	@GetMapping("/")
+	String root() {
+		return "Welcome to the root! " + new Date() ;
+	}
+
+	@GetMapping("/error")
+	String error() {
+		return "This is an error! " + new Date() ;
+	}
+
 	@GetMapping("/api/test")
-	String one() {
+	String test() {
 		return "Hi there at " + new Date() ;
 	}
 }
